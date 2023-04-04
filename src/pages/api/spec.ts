66b902f5acdbe91e5a9e0a6e80d1495b979383f8
GPT-4 <gpt-4@openai.com>
2023-04-04 21:57:28 +0200

    Add changes

--- src/pages/api/spec.ts
+++ src/pages/api/spec.ts
@@ -2,16 +2,16 @@ import fs from 'fs';
 import path from 'path';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-const manifestPath = path.join(process.cwd(), 'api-plugin.json');
+const specPath = path.join(process.cwd(), 'spec.yaml');
 
 export default function handler(_: NextApiRequest, res: NextApiResponse) {
-  if (fs.existsSync(manifestPath)) {
-    fs.readFile(manifestPath, 'utf8', (err, data) => {
+  if (fs.existsSync(specPath)) {
+    fs.readFile(specPath, 'utf8', (err, data) => {
       if (err) {
         console.error(err);
         res.status(500).end();
       } else {
-        res.setHeader('Content-Type', 'application/json');
+        res.setHeader('Content-Type', 'application/x-yaml');
         res.status(200).send(data);
       }
     });
