ed6618083a0771e6689448e93675fc8c6190a093
GPT-4 <gpt-4@openai.com>
2023-04-04 22:21:00 +0200

    Add serverless pupeteer

--- src/pages/api/render.ts
+++ src/pages/api/render.ts
@@ -1,15 +1,17 @@
 /* eslint-disable import/no-anonymous-default-export */
 // pages/api/render.ts
-import type { NextApiRequest, NextApiResponse } from 'next';
-import puppeteer from 'puppeteer';
-import fs from 'fs';
-import path from 'path';
-import { v4 as uuidv4 } from 'uuid';
+import type { NextApiRequest, NextApiResponse } from "next";
+import puppeteerCore from "puppeteer-core";
+import puppeteer from "puppeteer";
+import chromium from "chrome-aws-lambda";
+import fs from "fs";
+import path from "path";
+import { v4 as uuidv4 } from "uuid";
 
 const ONE_WEEK_MS = 7 * 24 * 60 * 60 * 1000;
 
 const cleanupTempImages = async () => {
-  const tempDir = path.join(process.cwd(), 'public', 'temp');
+  const tempDir = path.join(process.cwd(), "public", "temp");
   const files = await fs.promises.readdir(tempDir);
 
   const currentTime = Date.now();
@@ -30,11 +32,21 @@ const renderImage = async (
   javascript: string,
   viewportWidth: number,
   viewportHeight: number,
-  imageFormat: 'jpeg' | 'png',
+  imageFormat: "jpeg" | "png"
 ): Promise<string> => {
-  const browser = await puppeteer.launch({
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
-  });
+  let browser = {} as any;
+
+  if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+    browser = await puppeteerCore.launch({
+      args: chromium.args,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
+    });
+  } else {
+    browser = await puppeteer.launch({
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
+  }
 
   const page = await browser.newPage();
   await page.setViewport({ width: viewportWidth, height: viewportHeight });
@@ -44,7 +56,7 @@ const renderImage = async (
     <script>${javascript}</script>
   `);
 
-  const filename = uuidv4() + '.' + imageFormat;
+  const filename = uuidv4() + "." + imageFormat;
   const filepath = `./public/temp/${filename}`;
   const publicUrl = `/temp/${filename}`;
 
@@ -59,14 +71,14 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   const {
     html,
     css,
-    javascript = '',
-    viewportWidth = 1280,
-    viewportHeight = 720,
-    imageFormat = 'png',
+    javascript = "",
+    viewportWidth = 640,
+    viewportHeight = 640,
+    imageFormat = "png",
   } = req.body;
 
   if (!html || !css) {
-    return res.status(400).json({ message: 'HTML and CSS are required.' });
+    return res.status(400).json({ message: "HTML and CSS are required." });
   }
 
   try {
@@ -76,11 +88,11 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       javascript,
       viewportWidth,
       viewportHeight,
-      imageFormat,
+      imageFormat
     );
     return res.status(200).json({ imageUrl });
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ message: 'Error rendering image.' });
+    return res.status(500).json({ message: "Error rendering image." });
   }
 };
